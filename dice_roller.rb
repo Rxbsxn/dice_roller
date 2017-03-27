620d8068d23ea7e9580fb98246c46de7405c173f
Robert Harężlak <harezlak.robert7@gmail.com>
2017-03-27 23:10:17 +0200

    first iteration

---
+++ dice_roller.rb
@@ -0,0 +1,3 @@
+require_relative 'lib/dice'
+
+puts Dice.twenty_sides
---
+++ lib/dice.rb
@@ -0,0 +1,9 @@
+class Dice
+  def self.twenty_sides
+    rand(20) + 1
+  end
+
+  def self.thirty_sides
+    rand(30) + 1
+  end
+end
